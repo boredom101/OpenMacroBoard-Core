9576fcb5b11ba3e87f87be5e084cc2834f288222
Christian Wischenbart <christian.wischenbart@dataformers.at>
2018-08-29 17:10:47 +0200

    fsaafs

--- src/OpenMacroBoard.SDK/BitmapConvertions.cs
+++ src/OpenMacroBoard.SDK/BitmapConvertions.cs
@@ -1,112 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
+using static OpenMacroBoard.SDK.PixelFormatConvertion;
 
 namespace OpenMacroBoard.SDK
 {
-    internal static class DefaultBitmapConvertions
+    public unsafe static class PixelFormatConvertion
     {
-        public static void Register()
-        {
-            BitmapConvertions.RegisterConvertion<Bgra32, Bgr24>(Transform_Brga32ToBgr24);
-        }
-
-        private unsafe static void Transform_Bgr24ToBgr24(IntPtr sourceData, int sourceStart, IntPtr targetData, int targetStart)
-        {
-            var src = (byte*)sourceData;
-            var tar = (byte*)targetData;
-
-            tar[targetStart + 0] = src[sourceStart + 0];
-            tar[targetStart + 1] = src[sourceStart + 1];
-            tar[targetStart + 2] = src[sourceStart + 2];
-        }
-
-        private unsafe static void Transform_Brga32ToBgr24(IntPtr sourceData, int sourceStart, IntPtr targetData, int targetStart)
-        {
-            var src = (byte*)sourceData;
-            var tar = (byte*)targetData;
-
-            double alpha = (double)src[sourceStart + 3] / 255f;
-            tar[targetStart + 0] = (byte)Math.Round(src[sourceStart + 0] * alpha);
-            tar[targetStart + 1] = (byte)Math.Round(src[sourceStart + 1] * alpha);
-            tar[targetStart + 2] = (byte)Math.Round(src[sourceStart + 2] * alpha);
-        }
+        public delegate void PixelConvert(byte* sourceData, int sourceStart, byte* targetData, int targetStart);
     }
 
-    internal class FixStrideConverter
+    public sealed class BitmapConverter
     {
-        public FixStrideConverter()
+        private readonly Dictionary<(IPixelFormat source, IPixelFormat target), PixelConvert> registeredConverters
+            = new Dictionary<(IPixelFormat source, IPixelFormat target), PixelConvert>();
+
+        public BitmapConverter()
         {
 
         }
 
-        private unsafe static void Transform_Bgr24ToBgr24(IntPtr sourceData, int sourceStart, IntPtr targetData, int targetStart)
+        public void Register(IPixelFormat sourceFormat, IPixelFormat targetFormat, PixelConvert convertAction)
         {
-            var src = (byte*)sourceData;
-            var tar = (byte*)targetData;
+            var key = (sourceFormat, targetFormat);
+            registeredConverters.Add(key, convertAction);
+        }
 
-            tar[targetStart + 0] = src[sourceStart + 0];
-            tar[targetStart + 1] = src[sourceStart + 1];
-            tar[targetStart + 2] = src[sourceStart + 2];
+        public PixelConvert FindConverterFor(IPixelFormat sourceFormat, IPixelFormat targetFormat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public KeyBitmap Convert(KeyBitmap bitmap, IPixelFormat targetFormat)
+        {
+            var converter = FindConverterFor(bitmap.Format, targetFormat);
+            throw new NotImplementedException();
+        }
+
+        public unsafe static void BitmapTransformation(
+           int width, int height,
+           byte[] srcArray, int srcStride, int srcBytePerPixel,
+           byte[] tarArray, int tarStride, int tarBytePerPixel,
+           PixelConvert tranformationAction
+        )
+        {
+            fixed (byte* srcData = srcArray)
+            fixed (byte* tarData = srcArray)
+            {
+                for (int y = 0; y < height; y++)
+                    for (int x = 0; x < width; x++)
+                    {
+                        var startSource = srcStride * y + x * srcBytePerPixel;
+                        var startTarget = tarStride * y + x * tarBytePerPixel;
+                        tranformationAction(srcData, startSource, tarData, startTarget);
+                    }
+            }
         }
     }
 
     internal unsafe static class BitmapConvertions
     {
+        public static BitmapConverter Default { get; }
+
         static BitmapConvertions()
         {
-            DefaultBitmapConvertions.Register();
-        }
-
-        public static void RegisterConvertion<TSource, TTarget>(TAction convertAction)
-            where TSource : IPixelFormat
-            where TTarget : IPixelFormat
-        {
-
-        }
-
-
-
-        public static ITransformationAction Bgr24ToBgr24 { get; } = new TransformationAction(Transform_Bgr24ToBgr24);
-        public static ITransformationAction Bgra32ToBgr24 { get; } = new TransformationAction(Transform_Bgr24ToBgr24);
-
-        public interface ITransformationAction { }
-
-        private class TransformationAction : ITransformationAction
-        {
-            public UnsafeTransformationAction action;
-
-            public TransformationAction(UnsafeTransformationAction action)
-            {
-                this.action = action ?? throw new ArgumentNullException(nameof(action));
-            }
-        }
-
-        public delegate void TAction(IntPtr sourceData, int sourceStart, IntPtr targetData, int targetStart);
-
-        public delegate void UnsafeTransformationAction(byte* sourceData, int sourceStart, byte* targetData, int targetStart);
-
-        /// <summary>
-        /// This transformation can be used to remap images with different strides
-        /// </summary>
-        /// <param name="sourceData"></param>
-        /// <param name="sourceStart"></param>
-        /// <param name="targetData"></param>
-        /// <param name="targetStart"></param>
-        private static void Transform_Bgr24ToBgr24(byte* sourceData, int sourceStart, byte* targetData, int targetStart)
-        {
-            targetData[targetStart + 0] = sourceData[sourceStart + 0];
-            targetData[targetStart + 1] = sourceData[sourceStart + 1];
-            targetData[targetStart + 2] = sourceData[sourceStart + 2];
-        }
-
-        private static void Transform_Brga32ToBgr24(byte* sourceData, int sourceStart, byte* targetData, int targetStart)
-        {
-            double alpha = (double)sourceData[sourceStart + 3] / 255f;
-            targetData[targetStart + 0] = (byte)Math.Round(sourceData[sourceStart + 0] * alpha);
-            targetData[targetStart + 1] = (byte)Math.Round(sourceData[sourceStart + 1] * alpha);
-            targetData[targetStart + 2] = (byte)Math.Round(sourceData[sourceStart + 2] * alpha);
+            Default = new BitmapConverter();
+            Default.RegisterDefaults();
         }
 
         public static KeyBitmap ToRawBgr24(this Bitmap bitmap)
@@ -114,12 +75,13 @@ namespace OpenMacroBoard.SDK
             var w = bitmap.Width;
             var h = bitmap.Height;
 
-            System.Drawing.Imaging.BitmapData data = null;
+            BitmapData data = null;
             try
             {
                 data = bitmap.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly, bitmap.PixelFormat);
 
-                var targetFormat = PixelFormats.Bgr24;
+
+
                 var targetStride = targetFormat.BytesPerPixel * w;
 
                 var targetRawBitmap = new KeyBitmap(w, h, targetStride, targetFormat);
@@ -148,61 +110,6 @@ namespace OpenMacroBoard.SDK
                 if (data != null)
                     bitmap.UnlockBits(data);
             }
-        }
-
-        private static UnsafeTransformationAction FindTransformationForPair(PixelFormat originalFormat, IPixelFormat targetFormat)
-        {
-            if (targetFormat != PixelFormats.Bgr24)
-                throw new NotImplementedException();
-
-            switch (originalFormat)
-            {
-                case PixelFormat.Format24bppRgb: return Transform_Bgr24ToBgr24;
-                case PixelFormat.Format32bppArgb: return Transform_Brga32ToBgr24;
-                default:
-                    throw new NotSupportedException();
-            }
-        }
-
-        public static void BitmapTransformation(
-            int width, int height,
-            byte[] srcArray, int srcStride, int srcBytePerPixel,
-            byte[] tarArray, int tarStride, int tarBytePerPixel,
-            ITransformationAction tranformationAction
-        )
-        {
-            var tranform = tranformationAction as TransformationAction;
-            if (tranform == null)
-                throw new ArgumentException($"TransformationAction is not of Type {nameof(TransformationAction)}");
-            var action = tranform.action;
-
-            fixed (byte* srcData = srcArray)
-            fixed (byte* tarData = srcArray)
-            {
-                for (int y = 0; y < height; y++)
-                    for (int x = 0; x < width; x++)
-                    {
-                        var startSource = srcStride * y + x * srcBytePerPixel;
-                        var startTarget = tarStride * y + x * tarBytePerPixel;
-                        action(srcData, startSource, tarData, startTarget);
-                    }
-            }
-        }
-
-        private static void BitmapTransformation(
-            int width, int height,
-            byte* srcData, int srcStride, int srcBytePerPixel,
-            byte* tarData, int tarStride, int tarBytePerPixel,
-            UnsafeTransformationAction tranformationAction
-        )
-        {
-            for (int y = 0; y < height; y++)
-                for (int x = 0; x < width; x++)
-                {
-                    var startSource = srcStride * y + x * srcBytePerPixel;
-                    var startTarget = tarStride * y + x * tarBytePerPixel;
-                    tranformationAction(srcData, startSource, tarData, startTarget);
-                }
         }
     }
 }
--- src/OpenMacroBoard.SDK/PixelFormats.cs
+++ src/OpenMacroBoard.SDK/PixelFormats.cs
@@ -6,23 +6,6 @@
         public static Bgra32 Bgra32 { get; } = new Bgra32();
 
         public static bool Equals(IPixelFormat a, IPixelFormat b)
-        {
-            if (ReferenceEquals(a, b))
-                return true;
-
-            if (a is null)
-                return false;
-
-            if (b is null)
-                return false;
-
-            if (a.BytesPerPixel != b.BytesPerPixel)
-                return false;
-
-            if (!a.GetType().Equals(b.GetType()))
-                return false;
-
-            return true;
-        }
+            => ReferenceEquals(a, b);
     }
 }
